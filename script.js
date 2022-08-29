94f7c6fd6d3547650d64392d390d1cf09ad077ed
vit-krizka <vit.krizka@gmail.com>
2022-08-29 19:43:40 +0200

    logo - změna barvy po najetí

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//otvírání a zavírání kontaktního boxu
+
 function openContactBox() {
     let contactWindow = document.getElementById("window-contact");
     contactWindow.style.display = "block";
@@ -7,3 +9,33 @@ function closeContactBox() {
     let contactWindow = document.getElementById("window-contact");
     contactWindow.style.display = "none";
 }
+
+//akce po najetí na navbar-brand
+
+function changeLogoToColor() {
+    let image = document.getElementById("logoImage");
+    let textShort = document.getElementById("logoTextShort");
+    let textLong = document.getElementById("logoTextLong");
+    
+    image.src = "/assets/images/logo-color.png";
+    textShort.style.color = "grey";
+    textLong.style.color = "grey";
+}
+
+function changeLogoBack() {
+    let image = document.getElementById("logoImage");
+    let textShort = document.getElementById("logoTextShort");
+    let textLong = document.getElementById("logoTextLong");
+
+    image.src = "/assets/images/logo.png";
+    textShort.style.color = "white";
+    textLong.style.color = "white";
+}
+
+const navbarBrand = document.getElementById('navbar-brand');
+
+navbarBrand.addEventListener('mouseover', changeLogoToColor);
+navbarBrand.addEventListener('mouseout', changeLogoBack);
+// navbarBrand.addEventListener('touchstart', changeLogoToColor);
+// navbarBrand.addEventListener('touchmove', changeLogoToColor);
+// navbarBrand.addEventListener('touchend', changeLogoBack);
